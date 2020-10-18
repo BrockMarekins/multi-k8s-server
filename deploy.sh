4cafc052a20e3474a43ad41c2315f29c27d5de02
BrockMarekins <33862913+BrockMarekins@users.noreply.github.com>
2020-10-18 19:24:03 +0200

    Add old version (#1)

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,7 @@
-#kubectl apply -f k8s
+IMAGE=brockmarekins/multi-k8s-server
+
+docker build -t "$IMAGE":latest -t "$IMAGE":"$SHA" .
+docker push "$IMAGE":latest
+docker push "$IMAGE":"$SHA"
+
+kubectl set image deployments/server-deployment server="$IMAGE":"$SHA"
